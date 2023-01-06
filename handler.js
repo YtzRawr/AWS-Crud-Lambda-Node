87469ca10ff8e4cdfb0a9530e1a929c0c8d105ce
YtzRawr <matiaspizarro133@gmail.com>
2023-01-06 20:50:55 -0300

    add region sao paulo

--- handler.js
+++ handler.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  // code
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: 'Chanchito feliz!',
         input: event,
       },
       null,
