5da11696218276895620588d7c53ebd4b1108230
YtzRawr <matiaspizarro133@gmail.com>
2023-01-12 14:36:09 -0300

    falta arreglar error 500 internal server error

--- src/addTask.js
+++ src/addTask.js
@@ -1,12 +1,39 @@
-const { v4} = require('uuid')
-// se crea la fun
-const addTask = (event) =>{
-    // se reciben los datos
-    const {title, description} = event.body
-    const createAt = new Date()
-    const id = v4()
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const middy = require("@middy/core");
+const httpJSONBodyParser = require("@middy/http-json-body-parser");
+
+const addTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { title, description } = event.body;
+  const createdAt = new Date();
+  const id = v4();
+
+  console.log("created id: ", id);
+
+  const newTask = {
+    id,
+    title,
+    description,
+    createdAt,
+    done: false,
+  };
+
+  await dynamodb
+    .put({
+      TableName: "TaskTable",
+      Item: newTask,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTask),
+  };
 };
-// se exporta el mod
+
 module.exports = {
-    addTask,
+  addTask: middy(addTask).use(httpJSONBodyParser()),
 };
